d772231aeda910eb7f4cb4e57d530a033bd0ca61
fushuang <fushuang@didiglobal.com>
2020-05-07 17:01:51 +0800

    三种自定义Gradle插件的方式 1.当前build文件，享有groovy语法定义  继承Plugin 2.buildSrc 下定义，buildSrc位于Project目录，整个项目全局可用 3.放到单独的Module里，把Jar发布到Maven，不同项目可以依赖  	1）定义插件类 	2）通过com.fs.plugin.MyBuildScripPlugin.properties 中implementation-class 声明插件名称（一个插件库可以定义多个插件），.properties前面的名称即为用户apply plugin: 后面的名称 	3）发布到本地Maven仓库，{groupId + artifactId + version} 即为classpath 依赖的jar包Path 路径（可以看下mevenven本地的文件路径就知道了，他在maven根路径下根据这些id创建每一个库文件路径）

---
+++ gradlePublish/src/main/groovy/com/gradle/plugin/MyBuildScripPluginSingle.java
@@ -0,0 +1,27 @@
+package com.gradle.plugin;
+
+import org.gradle.api.Action;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+
+/**
+ * Author: fushuang
+ * Date: 2020/5/7 下午3:23
+ * Description:
+ * Wiki:
+ * History:
+ * <author> <time> <version> <desc>
+ */
+public class MyBuildScripPluginSingle implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+        project.task("myPluginSingle", new Action<Task>() {
+            @Override
+            public void execute(Task task) {
+                System.out.println(">>>>>>>>>> myPluginSingle log tag <<<<<<<<<<<");
+            }
+        });
+    }
+}
