a8f32ba918f4f22f32ba573e0aa82c2b05b15bca
fushuang <fushuangfushuang@didiglobal.com>
2020-08-12 17:49:51 +0800

    更新文档

--- app/src/androidTest/java/com/example/gradlejavademo/AMSTest.java
+++ app/src/androidTest/java/com/example/gradlejavademo/AMSTest.java
@@ -7,8 +7,16 @@ public class AMSTest {
     }
 
     public static void test(String name){
+        AMSTest amsTest = new AMSTest();
+        int result = (int) amsTest.add(123, 456.2f);
 
-        System.out.println(String.format("visit method : %s",name));
+        System.out.println(String.format("result: %f",result));
+
+//        System.out.println(String.format("visit method : %s",name));
 //        System.out.println("aaa");
+    }
+
+    public  float add(int a,float b){
+       return a+b;
     }
 }
